aeb41e504be9b1bd7906e043b763cf453158dc92
lekimi <kimi0225@qq.com>
2026-09-22 23:14:50 +0800

    更新 data/dict/index.js

---
+++ data/dict/index.js
@@ -0,0 +1,3 @@
+/* 自动生成：词典分片清单 */
+window.DICT_KEYS=["0", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+window.DICT_META={"source": "ECDICT (MIT)", "core": 13878, "total": 767807, "builtAt": ""};
